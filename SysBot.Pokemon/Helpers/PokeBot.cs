7c1ec6a99723b863cf0cbe7940a6f20b0d5c1972
Taku1991 <139764599+Taku1991@users.noreply.github.com>
2025-12-26 19:56:21 +0100

    change workflow

--- SysBot.Pokemon/Helpers/PokeBot.cs
+++ SysBot.Pokemon/Helpers/PokeBot.cs
@@ -8,7 +8,7 @@ public static class PokeBot
 
     public const string ConfigPath = "config.json";
 
-    public const string Version = "v7.0.7a";
+    public const string Version = "v7.0.7b";
 
     /// <summary>
     /// Checks if a user can use AutoOT functionality.
