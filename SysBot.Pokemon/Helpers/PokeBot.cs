f7f7e47a6d1c4c9ad1a1ff299228f2995dc303d7
Secludedly <GogoplataSteve@Gmail.com>
2026-03-19 20:51:02 -0400

    Dependencies Update + PLZA 2.0.2 Support
    v7.6.3
    
    - PKHeX.Core from 03-19-2026 (commit 0e097b1)
    - PKHeX.Core.AutoMod from 03-19-2026 (commit df0fd59)
    
    - PLZAGameVersion set to 2.0.2.

--- SysBot.Pokemon/Helpers/PokeBot.cs
+++ SysBot.Pokemon/Helpers/PokeBot.cs
@@ -8,7 +8,7 @@ public static class PokeBot
 
     public const string ConfigPath = "config.json";
 
-    public const string Version = "v7.6.1-LXC-3";
+    public const string Version = "v7.6.4-LXC";
 
     /// <summary>
     /// Checks if a user can use AutoOT functionality.
