8aadc7d2f9a2ce2f21e8fb8d81174b0533b1eccc
Taku1991 <139764599+Taku1991@users.noreply.github.com>
2026-04-12 17:31:02 +0200

    merge upstream

--- SysBot.Pokemon/Helpers/PokeBot.cs
+++ SysBot.Pokemon/Helpers/PokeBot.cs
@@ -8,7 +8,7 @@ public static class PokeBot
 
     public const string ConfigPath = "config.json";
 
-    public const string Version = "v7.7.0-LXC";
+    public const string Version = "v7.8.2-LXC";
 
     /// <summary>
     /// Checks if a user can use AutoOT functionality.
