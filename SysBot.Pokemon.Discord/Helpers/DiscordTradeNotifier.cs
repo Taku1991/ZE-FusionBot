955a789480dcf1bb2c4261f235fff935263fba4e
Taku1991 <139764599+Taku1991@users.noreply.github.com>
2026-05-11 21:13:55 +0200

    fix: remove Windows-only CreateLGLinkCodeSpriteEmbed, SkiaSharp version already in use

--- SysBot.Pokemon.Discord/Helpers/DiscordTradeNotifier.cs
+++ SysBot.Pokemon.Discord/Helpers/DiscordTradeNotifier.cs
@@ -387,56 +387,6 @@ public class DiscordTradeNotifier<T> : IPokeTradeNotifier<T>, IDisposable
         Trader.SendMessageAsync(msg, embed: embed.Build()).ConfigureAwait(false);
     }
 
-    public static (string, Embed) CreateLGLinkCodeSpriteEmbed(List<Pictocodes> lgcode)
-    {
-        int codecount = 0;
-        List<System.Drawing.Image> spritearray = [];
-        foreach (Pictocodes cd in lgcode)
-        {
-            var showdown = new ShowdownSet(cd.ToString());
-            var sav = BlankSaveFile.Get(EntityContext.Gen7b, "pip");
-            PKM pk = sav.GetLegalFromSet(showdown).Created;
-            var sprite = pk.Sprite();
-            var destRect = new Rectangle(-40, -65, 137, 130);
-            var destImage = new Bitmap(137, 130);
-            destImage.SetResolution(sprite.HorizontalResolution, sprite.VerticalResolution);
-            using (var graphics = Graphics.FromImage(destImage))
-            {
-                graphics.CompositingMode = System.Drawing.Drawing2D.CompositingMode.SourceCopy;
-                graphics.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
-                graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
-                graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
-                graphics.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
-                graphics.DrawImage(sprite, destRect, 0, 0, sprite.Width, sprite.Height, GraphicsUnit.Pixel);
-            }
-            sprite.Dispose();
-            spritearray.Add(destImage);
-            codecount++;
-        }
-        int outputImageWidth = spritearray[0].Width + 20;
-
-        int outputImageHeight = spritearray[0].Height - 65;
-
-        using var outputImage = new Bitmap(outputImageWidth, outputImageHeight, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
-
-        using (Graphics graphics = Graphics.FromImage(outputImage))
-        {
-            graphics.DrawImage(spritearray[0], new Rectangle(0, 0, spritearray[0].Width, spritearray[0].Height),
-                new Rectangle(new Point(), spritearray[0].Size), GraphicsUnit.Pixel);
-            graphics.DrawImage(spritearray[1], new Rectangle(50, 0, spritearray[1].Width, spritearray[1].Height),
-                new Rectangle(new Point(), spritearray[1].Size), GraphicsUnit.Pixel);
-            graphics.DrawImage(spritearray[2], new Rectangle(100, 0, spritearray[2].Width, spritearray[2].Height),
-                new Rectangle(new Point(), spritearray[2].Size), GraphicsUnit.Pixel);
-        }
-        foreach (var img in spritearray)
-            img.Dispose();
-        var filename = $"{System.IO.Directory.GetCurrentDirectory()}//finalcode.png";
-        outputImage.Save(filename);
-        filename = System.IO.Path.GetFileName($"{System.IO.Directory.GetCurrentDirectory()}//finalcode.png");
-        Embed returnembed = new EmbedBuilder().WithTitle($"{lgcode[0]}, {lgcode[1]}, {lgcode[2]}").WithImageUrl($"attachment://{filename}").Build();
-        return (filename, returnembed);
-    }
-
     public void Dispose()
     {
         StopPeriodicUpdates();
